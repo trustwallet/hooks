54a1ea0cc44c7e8e74f4ff356aeae210360d63ad
tw-calvin <calvin.a@trustwallet.com>
2024-12-13 16:00:50 +0800

    💽️ chore: add comments to setup-hooks

--- setup-hooks.sh
+++ setup-hooks.sh
@@ -1,3 +1,4 @@
+# 1. Define script to run as: init, post-checkout, post-merge
 read -r -d '' PRE_COMMIT_HOOK << EOL
 #!/bin/sh
 
@@ -10,9 +11,12 @@ pre-commit install
 pre-commit install --hook-type commit-msg
 EOL
 
+# 2. Set script to post-checkout & post-merge
+#    This will pre-commit changes will be applied on git pull
 echo "$PRE_COMMIT_HOOK" > .git/hooks/post-checkout
 chmod +x .git/hooks/post-checkout
 echo "$PRE_COMMIT_HOOK" > .git/hooks/post-merge
 chmod +x .git/hooks/post-merge
 
+# 3. Trigger initial script run
 bash <<< "$PRE_COMMIT_HOOK"
